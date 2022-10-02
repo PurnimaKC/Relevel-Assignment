efb536ae87122ec60149adf3500d4bee5f2ab6c5
PurnimaKC <kpurnimac@gmail.com>
2022-10-02 18:11:41 +0530

    Completed Assignment 1 and Assignment 2

---
+++ Basic Constructs- Intro to Functions and Scopes/Assignment 1/script.js
@@ -0,0 +1,4 @@
+function astroGuru (noc,spouse,location,job) {
+    console.log(`You will be a ${job} in ${location}, and married to ${spouse} with ${noc} kids.`);
+}
+astroGuru(1,"Ramesh","Pune","SWE");
---
+++ Basic Constructs- Intro to Functions and Scopes/Assignment 2/script.js
@@ -0,0 +1,10 @@
+function max (number1,number2) {
+    if(number1 > number2)
+       return number1;
+    else if(number1 < number2)
+       return number2;
+    else
+        return number1;
+}
+ 
+ console.log(max(1011,105));
